af14771f2f3438f3f9567f9566c956fc483c89ed
Bao-gio-co-tien <129248299+Bao-gio-co-tien@users.noreply.github.com>
2024-03-14 07:58:24 +0700

    Add files via upload

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,19 @@
-public class Book {
+public class Book extends Item{
+    String author;
+    int numberOfPages;
+    String genre;
+
+    @Override
+    public void showInfo() {
+        System.out.println("Day la mot so thong tin cua cuon sach nay: ");
+        System.out.println("Ten san pham: " + name +
+                ", ID san pham: " + ID + ", ten tac gia: " + author + ", so trang: " + numberOfPages +
+                ", the loai: " + genre + ", gia thanh: " + price + "$");
+        System.out.println(getDescription());
+    }
+    Book(String name, String description, String ID, float price, String author, int numberOfPages, String genre) {
+        super(name, description, ID, price);
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+    }
 }
--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,17 @@
-public class MP3 {
+public class MP3 extends Item{
+    int duration;
+    @Override
+    public void showInfo() {
+        System.out.println("Day la mot so thong tin cua chiec MP3 nay: ");
+        System.out.println("Ten san pham: " + name +
+                ", ID san pham: " + ID + ", thoi gian: " + duration + "s, " +
+                ", gia thanh: " + price + "$");
+        System.out.println(getDescription());
+    }
+
+    MP3(String name, String description, String ID, float price, int duration) {
+        super(name, description, ID, price);
+        this.duration = duration;
+    }
+
 }
