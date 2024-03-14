9550d1436ceff26edb83da9471324819d1882513
Bao-gio-co-tien <129248299+Bao-gio-co-tien@users.noreply.github.com>
2024-03-14 08:01:14 +0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3 = new MP3("BR3000", "Day la mot san pham co nhieu tinh nang dot pha thoi dai",
+        Item mp3 = new MP3("BR3000", "Day la mot san pham co nhieu tinh nang dot pha thoi dai",
                 "BR#03", 34, 40000);
         mp3.showInfo();
 
-        Book book = new Book("Giao trinh Chu nghia xa hoi khoa hoc", "Giao trinh the hien nhieu ket qua nghien cuu moi ve chu nghia xa hoi khoa hoc",
+        Item book = new Book("Giao trinh Chu nghia xa hoi khoa hoc", "Giao trinh the hien nhieu ket qua nghien cuu moi ve chu nghia xa hoi khoa hoc",
                 "GT#01", 3, "Bo Giao duc va Dao tao", 276, "Giao trinh hoc");
         book.showInfo();
 
